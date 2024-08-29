55a3814948d57320309747c4f6d7579133c150c5
aswin-bs <aswinbs1452@gmail.com>
2024-08-29 17:49:32 +0530

    .

---
+++ TUF/binart_single_occ.py
@@ -0,0 +1,30 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        
+        if len(nums) == 1 : return nums[0]
+
+        if nums[len(nums)-1] != nums[len(nums)-2] : return nums[len(nums)-1]
+        l , r  = 0 , len(nums) - 1
+
+        while(l<=r):
+
+            
+
+            mid = l + (r-l)//2
+
+            if mid%2 == 1 and mid != 0:
+                mid = mid -1
+            
+            if nums[mid -1] != nums[mid] and nums[mid] != nums[mid+1]:
+                return nums[mid]
+
+            if nums[mid-1] == nums[mid]:
+                r = mid -1
+            else:
+                l = mid + 1
+
+
+
+
+        
+        
